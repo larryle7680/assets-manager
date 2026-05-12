938276f885a677c4cb277c264832c865511a3ce2
Larry <larryle7680@gmail.com>
2026-05-12 14:20:08 -0500

    Created Asset Class. Added Attributes to Assets Added Constructor for Asset Added getValue() Method.

---
+++ src/main/java/com/pluralsight/Asset.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Asset {
+
+    //Attributes of Assets
+    private String description;
+    private String dateAcquired;
+    private double originalCost;
+
+    //Constructor
+    public Asset(String description, String dateAcquired, double originalCost) {
+        this.description = description;
+        this.dateAcquired = dateAcquired;
+        this.originalCost = originalCost;
+    }
+
+    //Method inside of Asset
+    public double getValue(){
+        return originalCost;
+    }
+}
