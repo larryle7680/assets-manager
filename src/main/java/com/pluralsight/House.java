8202c725e725033e5ed6e50c4736da15e584b28f
Larry <larryle7680@gmail.com>
2026-05-12 14:33:54 -0500

    Created House Class. Added Attributes to House Added Constructor to House Added Override getValue() Method to House.

---
+++ src/main/java/com/pluralsight/House.java
@@ -0,0 +1,62 @@
+package com.pluralsight;
+
+public class House extends Asset{
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    //Constructor
+    public House(String description, String dateAcquired, double originalCost) {
+        super(description, dateAcquired, originalCost);
+    }
+
+    //Setters/Getters
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    @Override
+    public double getValue(){
+        if(condition == 1){
+           return (180 * this.squareFoot) + (.25 * this.lotSize);
+        }else if (condition == 2){
+            return (130 * this.squareFoot) + (.25 * this.lotSize);
+        }else if (condition == 3){
+            return (90 * this.squareFoot) + (.25 * this.lotSize);
+        }else if (condition == 4){
+            return (condition * this.squareFoot) + (.25 * this.lotSize);
+        }
+
+        return getValue();
+    }
+}
